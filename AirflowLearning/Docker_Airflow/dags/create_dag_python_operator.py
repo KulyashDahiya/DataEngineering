8410895d58c6dd50433398bfc414c86686d0d4fb
Kulyash Dahiya <kulyashdahiya77@gmail.com>
2024-12-19 13:16:35 +0530

    Airflow

---
+++ AirflowLearning/Docker_Airflow/dags/create_dag_python_operator.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner' : 'code2j',
+    'retries' : 5,
+    'retry_delay' : timedelta(minutes = 5)
+}
+
+def greet(age, ti):
+    first_name = ti.xcom_pull(task_ids='Push_Name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='Push_Name', key='last_name')
+    print(f"Hello world, My name is {first_name} {last_name}, I am {age} years old.")
+
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Kulyash')
+    ti.xcom_push(key='last_name', value='Dahiya')
+
+with DAG(
+    default_args = default_args,
+    dag_id = 'our_dag_with_python_operator_v03',
+    description = 'Our first dag using python operator',
+    start_date = datetime(2024, 12, 17),
+    schedule_interval = '@daily'
+) as dag:
+
+    task1 = PythonOperator(
+        task_id = "greet",
+        python_callable=greet,
+        op_kwargs={'age': 24}
+    )
+
+    task2 = PythonOperator(
+        task_id = "Push_Name",
+        python_callable = get_name,
+    )
+
+    task2>>task1
